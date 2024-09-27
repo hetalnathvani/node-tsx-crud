72cb7cb91f4f042cd600ea1bdcd0c840a38b7ec7
hetalnathvani <nathvanihetal@gmail.com>
2024-09-27 15:54:31 +0530

    Installed and set up express js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req);
+  console.log(res);
+
+  res.send("Hello, Node!");
+});
+
+const port = 3002;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
